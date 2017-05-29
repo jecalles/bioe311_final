e14deac74063d3d56e3e68bebd84a4622e5d6ebb
jecalles <callesjonathan@gmail.com>
2017-05-29 13:35:04 -0700

    created repressilator class

---
+++ matlab_files/repressilator.m
@@ -0,0 +1,20 @@
+classdef repressilator
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Main class for final project
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    properties 
+        species = []
+        parameters = []
+        sim_param = []   
+    end
+    
+    methods
+        function obj = repressilator()
+        end
+        
+        function out = run 
+        
+    end
+    
+end
